ebca7b293ebf16454db5ef93646d4a130a09210c
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-07-22 16:27:39 -0300

    Explicar conteudo lacos

---
+++ script.js
@@ -0,0 +1,24 @@
+let tristeza = `eu to muito triste, #AlguémMeHelpa`
+
+let contador = 0; 
+
+while(contador <55){
+    console.log(tristeza);
+    contador ++;
+}
+
+ let listaAleatoria = ["cookies", "chocolate", "risole", "coxinha", "coca cola", "carolinas", "sonho", "pizza", "lasanha"];
+;
+for(let contador = 0; contador<listaAleatoria.length; contador ++){
+    console.log(listaAleatoria[contador]);
+}
+
+for(let besteiras in listaAleatoria){
+    console.log(besteiras);
+}
+
+for(let comidas of listaAleatoria){
+      console.log(comidas);
+}
+
+
