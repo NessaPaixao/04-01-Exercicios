3fb8f7f1f3906ba085871072c096ad22ffe3c4ee
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-07-22 17:53:29 -0300

    Realizar exercicio sobre for in e for of

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ for(let comidas of listaAleatoria){
 }
 
 
+let nome = prompt("Digite o seu nome:");
+let idade = Number(prompt("Digite sua idade:"));
+let filmes = [
+    {nome: "Invocação do mal", classificacao: 16, genero: "terror"},
+    {nome: "Ponte para terabitia", classificacao: 10, genero: "drama"},
+    {nome: "Sr. e Sra. Smith", classificacao: 16, genero: "misterio"},
+    {nome: "Divertidamente", classificação: "livre", genero: "animacao"},
+    {nome: "Como eu era antes de você", classificacao: 12, genero: "romance"},
+    {nome: "A fantástica fabrica de chocolate", classificacao: "livre", genero: "animacao"},
+    {nome: "Nerve:Um jogo sem regras", classificacao: 12, genero: "drama/misterio"},
+    {nome: "Jogador Nº1", classificacao: 12, genero: "suspense"},
+    {nome: "Moana: Um mar de aventuras", classificacao: "livre", genero: "animacao"},
+]
+
+for(let filme of filmes){
+    if(idade >= filme.classificacao){
+        console.log(`${nome}, você pode assistir ao filme ${filme.nome} por ter ${idade} anos.`)
+    }
+}
